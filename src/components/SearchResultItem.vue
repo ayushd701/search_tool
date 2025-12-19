db658ab1be6355fdf47a9ab4ebd3e62248a83ef9
ayushd701 <dixitayush0107@gmail.com>
2025-12-19 23:06:11 +0530

    Item added with local toggling.

---
+++ src/components/SearchResultItem.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+})
+
+const isOpen = ref(false)
+
+function toggle() {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+  <div class="item">
+    <h4 @click="toggle">
+      {{ item.title }}
+    </h4>
+
+    <p v-show="isOpen">
+      {{ item.description }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  border-bottom: 1px solid #ddd;
+  padding: 10px 0;
+  cursor: pointer;
+}
+
+h4 {
+  margin: 0;
+}
+</style>
