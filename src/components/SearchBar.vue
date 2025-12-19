4de3f11b107af2879261babf1789eb5599057698
ayushd701 <dixitayush0107@gmail.com>
2025-12-19 21:55:55 +0530

    Searchbar component added.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const emit = defineEmits(['search'])
+
+const query = ref('')
+let debounceTimer = null
+
+watch(query, (newValue) => {
+  clearTimeout(debounceTimer)
+
+  debounceTimer = setTimeout(() => {
+    emit('search', newValue)
+  }, 300)
+})
+</script>
+
+<template>
+  <div class="search-bar">
+    <input
+      type="text"
+      v-model="query"
+      placeholder="Search..."
+    />
+  </div>
+</template>
+
+<style scoped>
+.search-bar {
+  width: 100%;
+}
+
+input {
+  width: 100%;
+  padding: 10px 12px;
+  font-size: 16px;
+}
+</style>
