c9787fac6f939267cc6f2396ba591f2c1bc8cfdf
ayushd701 <dixitayush0107@gmail.com>
2025-12-20 10:55:04 +0530

    Searchlist completed.

---
+++ src/components/SearchResultList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SearchResultItem from './SearchResultItem.vue'
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="result-list">
+    <SearchResultItem v-for="item in items" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<style scoped>
+.result-list {
+  margin-top: 16px;
+}
+</style>
